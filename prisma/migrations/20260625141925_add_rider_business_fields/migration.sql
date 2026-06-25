980e2c0a2884728e27a8ab892c57462cecfe3386
alph-leo <saidex1@ymail.com>
2026-06-25 15:25:57 +0100

    rider business fields migration

---
+++ prisma/migrations/20260625141925_add_rider_business_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `riderprofile` ADD COLUMN `businessAddress` VARCHAR(191) NULL,
+    ADD COLUMN `businessCity` VARCHAR(191) NULL,
+    ADD COLUMN `businessName` VARCHAR(191) NULL,
+    ADD COLUMN `businessState` VARCHAR(191) NULL;
